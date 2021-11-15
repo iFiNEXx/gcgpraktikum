2457e7ec11e2bb758df5b6933ae6ebd70fc32c50
Jan <jan.huels@study.hs-duesseldorf.de>
2021-11-15 16:03:30 +0100

    Update bresenham_1458.js

--- bresenham_1458.js
+++ bresenham_1458.js
@@ -24,38 +24,44 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-function drawLine(x0, y0, x1, y1){
-  let a = Math.abs(y1 - y0);
+function drawLine(x0, y0, x1, y1) {
+  /**
+   * Durch Math.abs() wird der Betrag von dem Ergebnis der Rechnung genommen
+   */
+  let a = Math.abs(y1 - y0); 
   let b = -Math.abs(x1 - x0);
   let q = 2*a+b;
   let q_step = 2*(a+b);
   let q_equal = 2*a;
   let y = y0;
   let x = x0;
-  let incx =1;
-  let incy =1;
+  let incx =1; //Inkrementierung auf der x-Achse (nach rechts positiv, nach links negativ)
+  let incy =1; //Inkrementierung auf der y-Achse (nach rechts positiv, nach links negativ)
 
-  if(x0>=x1){
+  if(x0 >= x1) {
     incx = -1;
   }
 
-  if(y0>=y1){
+  if(y0 >= y1) {
     incy = -1;
   }
-  
-  for(x; x !== x1; x += incx){
 
+  /*
+  * Dieser Teil zeichnet die Linie.
+  * Dadurch das wir nun incx/incy eingebaut (Sie ersetzen das ehemalige ++) haben funktionieren die Oktanten 4,5,8 nun auch.
+  **/
+  for(x; x !== x1; x += incx) {
       setPixel(x, y);
+
       if (y0 === y1) {
         break;
       }
+
       if(q < 0) {
         q = q + q_equal;
-      }
-
-      else {
+      } else {
         q = q + q_step;
-        y += incy;
+        y += incy; 
       }
   }
 }
@@ -67,11 +73,9 @@ function drawLine(x0, y0, x1, y1){
 // Diese Funktion dient als Codebeispiel.
 // Sie wird beim Laden der Seite aufgerufen und kann entfernt werden.
 ////////////////////////////////////////////////////////////////////////////////
-function example(i)
-{
+function example(i) {
   let y = i + 2;
-  for (let x = 0; x < 400; x++)
-  {
+  for (let x = 0; x < 400; x++) {
     y--;
     if (y < -i)
     {
