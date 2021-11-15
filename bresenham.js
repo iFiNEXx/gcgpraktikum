da4146b8dc0cec9c6466e1cd6a9f806daf40c11d
vaki2418 <emil.kiesche@gmail.com>
2021-11-15 16:31:34 +0100

    Alle anderen Oktanten

--- bresenham.js
+++ bresenham.js
@@ -28,15 +28,28 @@ function drawLine(x0, y0, x1, y1){
   /**
    * Durch Math.abs() wird der Betrag von dem Ergebnis der Rechnung genommen
    */
+
+   let swap = false;
+   if(Math.abs(y1 - y0) > Math.abs(x1 - x0) ) {
+    let tmp = x0;
+    x0 = y0;
+    y0 = tmp;
+    tmp= y1;
+    y1 = x1;
+    x1 = tmp;//.
+    swap = true;
+  }
+  let y = y0;
+  let x = x0;
   let a = Math.abs(y1 - y0); 
   let b = -Math.abs(x1 - x0);
   let q = 2*a+b;
   let q_step = 2*(a+b);
   let q_equal = 2*a;
-  let y = y0;
-  let x = x0;
+
   let incx =1; //Inkrementierung auf der x-Achse (nach rechts positiv, nach links negativ)
   let incy =1; //Inkrementierung auf der y-Achse (nach rechts positiv, nach links negativ)
+
 
   if(x0>=x1){
     incx = -1;
@@ -44,13 +57,19 @@ function drawLine(x0, y0, x1, y1){
   if(y0>=y1){
     incy = -1;
   }
+  
   /*
   * Dieser Teil zeichnet die Linie.
   * Dadurch das wir nun incx/incy eingebaut (Sie ersetzen das ehemalige ++) haben funktionieren die Oktanten 4,5,8 nun auch.
+  * Durch y
   **/
   for(x; x !== x1; x += incx){
-
+      if (swap) {
+        setPixel(y,x)
+      }
+      else {
       setPixel(x, y);
+      }
       if (y0 === y1) {
         break;
       }
