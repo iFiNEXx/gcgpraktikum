373cf62571a61d1128992805d4559ce37d5f3fda
Jan <jan.huels@study.hs-duesseldorf.de>
2021-11-15 15:12:37 +0100

    Copy bresenham.js to bresenham_1.js

---
+++ bresenham_1.js
@@ -0,0 +1,68 @@
+
+////////////////////////////////////////////////////////////////////////////////
+// bresenham.js
+//
+// Bearbeiten Sie diese Datei für den Praktikumsteil "Bresenham Line".
+//
+// HS Duesseldorf - Fachbereich Medien - Grundlagen d. Computergrafik
+//
+// Studiengang:
+// Gruppe     :
+// Autor 1    :
+// Autor 2    :
+// Autor 3    :
+// Autor 4    :
+// Autor 5    :
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// drawLine(x0, y0, x1, y1)
+// Diese Funktion soll eine Linie von (x0, y0) nach (x1, y1) zeichnen.
+// Implementieren Sie dazu den Bresenham-Line-Algorithmus für alle Oktanten
+// in dieser Funktion. Einen Punkt zeichnen Sie mit setPixel(x,y).
+////////////////////////////////////////////////////////////////////////////////
+
+
+function drawLine(x0, y0, x1, y1){
+  let a = y1 - y0;
+  let b = -(x1 - x0);
+  let q = 2*a+b;
+  let q_step = 2*(a+b);
+  let q_equal = 2*a;
+  let y = y0;
+
+  for(let x = x0; x <= x1; x++){
+      setPixel(x, y);
+
+      if(q < 0) {
+        q = q + q_equal;
+      }
+
+      else {
+        q = q + q_step;
+        y++;
+      }
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// example(i)
+// Diese Funktion dient als Codebeispiel.
+// Sie wird beim Laden der Seite aufgerufen und kann entfernt werden.
+////////////////////////////////////////////////////////////////////////////////
+function example(i)
+{
+  let y = i + 2;
+  for (let x = 0; x < 400; x++)
+  {
+    y--;
+    if (y < -i)
+    {
+      y = i;
+    }
+    setPixel(x, Math.abs(y));
+  }
+}
