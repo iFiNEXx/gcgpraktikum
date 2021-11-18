c7af28da461feea8a94a8aa8f8914ac7d9df34c2
vaki2418 <emil.kiesche@gmail.com>
2021-11-18 18:25:28 +0100

    kommentare eingebaut

--- bresenham_1458.js
+++ bresenham_1458.js
@@ -25,19 +25,31 @@
 
 
 function drawLine(x0, y0, x1, y1) {
+  
+  let x0 = start_x0; // Zuweisung der x0 Variable
+  let y0 = start_y0; // Zuweisung der y0 Variable
+  let x1 = end_x1; // Zuweisung der x1 Variable
+  let y1 = end_y1; // Zuweisung der y1 Variable
+  let y = y0; // Zuweisung der Y Variable für die Schleife
+  let x = x0; // Zuweisung der X Variable für die Schleife
+
   /**
    * Durch Math.abs() wird der Betrag von dem Ergebnis der Rechnung genommen
    */
-  let a = Math.abs(y1 - y0); 
-  let b = -Math.abs(x1 - x0);
-  let q = 2*a+b;
-  let q_step = 2*(a+b);
-  let q_equal = 2*a;
-  let y = y0;
-  let x = x0;
+  let delta_y = Math.abs(y1 - y0); // Zuweisung und berechnung von Delta Y
+  let delta_x = -Math.abs(x1 - x0); // Zuweisung und berechnung von Delta X
+  let q = 2 * delta_y + delta_x; // Zuweisung und berechnung von der Entscheidungsvariable Q
+  let q_step = 2 * (delta_y + delta_x); // Inkrement für die Entscheidungsvariable Q
+  let q_equal = 2 * delta_y; // Variable für die Berechnung des neuen Q Werts
+
+
   let incx =1; //Inkrementierung auf der x-Achse (nach rechts positiv, nach links negativ)
   let incy =1; //Inkrementierung auf der y-Achse (nach rechts positiv, nach links negativ)
 
+  /*
+  * Falls der Start X oder Y Wert größer als der End X oder Y Wert ist,
+  * dann wird das Inkrement negativ.
+  **/
   if(x0 >= x1) {
     incx = -1;
   }
@@ -47,7 +59,7 @@ function drawLine(x0, y0, x1, y1) {
   }
 
   /*
-  * Dieser Teil zeichnet die Linie.
+  * Dieser Teil zeichnet die Linie indem jeder einzelne Pixel gesetzt wird.
   * Dadurch das wir nun incx/incy eingebaut (Sie ersetzen das ehemalige ++) haben funktionieren die Oktanten 4,5,8 nun auch.
   **/
   for(x; x !== x1; x += incx) {
