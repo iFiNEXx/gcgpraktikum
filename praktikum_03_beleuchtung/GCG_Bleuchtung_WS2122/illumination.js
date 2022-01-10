6d8e35e5b0fa3abdacf8a28db9be139c7cda378c
vaki2418 <emil.kiesche@gmail.com>
2022-01-10 17:44:34 +0100

    alle aufgaben (wenig kommentare)

--- praktikum_03_beleuchtung/GCG_Bleuchtung_WS2122/illumination.js
+++ praktikum_03_beleuchtung/GCG_Bleuchtung_WS2122/illumination.js
@@ -21,13 +21,21 @@ let ambientLight = {intensity: {r: 0.125, g: 0.125, b: 0.125}};
 // ambiente Reflektionskoeffizient
 let ambientCoeffizient = {r: 1, g: 1, b: 1};
 // Diffuser Reflektionskoeffizient
-let diffuseCoeffizient = {r: 1, g: 1, b: 1};
+let diffuseCoeffizient = {r: 0.5, g: 0.5, b: 0.5};
+// Spektrum Reflektionskoeffizient
+let spectrumCoeffizient = {r: 1, g: 1, b: 1};
+//Exponent
+let n = 10;
 
 // alle übrigen (Punkt)-Lichter der Szene
 let lights = [
   //position 0
   {position: new THREE.Vector3(-100, 100, 75),
    intensity: {r: 0.875, g: 0.625, b: 0.125}},
+  {position: new THREE.Vector3(100, -100, 75),
+   intensity: {r: 0.5, g: 0.0, b: 0.9}},
+   {position: new THREE.Vector3(100, 100, 75),
+    intensity: {r: 0.1, g: 0.5, b: 0.1}},
 ];
 
 
@@ -62,14 +70,16 @@ function phong(position, normal, camPosition)
   // TODO: Implementieren Sie die Beleuchtungsberechnung
   //       mit dem Phong-Beleuchtungsmodell.
 
-  
+  //Ambient
   //Berechnung der Formel ia = Ia· ka
   outColor.r += ambientLight.intensity.r * ambientCoeffizient.r;
   outColor.g += ambientLight.intensity.g * ambientCoeffizient.g;
   outColor.b += ambientLight.intensity.b * ambientCoeffizient.b;
 
+  //Diffuse
+  for (let i=0; i<lights.length; i++) {
   let lightVec = new THREE.Vector3();
-  lightVec.subVectors(lights[0].position, position);
+  lightVec.subVectors(lights[i].position, position);
   // normailisieren
   lightVec.normalize();
   let normalVec = new THREE.Vector3().copy(normal.normalize());
@@ -77,12 +87,31 @@ function phong(position, normal, camPosition)
   let scalar = normalVec.dot(lightVec);
 
   if (scalar >= 0) {
-    outColor.r += lights[0].intensity.r * diffuseCoeffizient.r * scalar;
-    outColor.g += lights[0].intensity.g * diffuseCoeffizient.g * scalar;
-    outColor.b += lights[0].intensity.b * diffuseCoeffizient.b * scalar;
+    outColor.r += lights[i].intensity.r * diffuseCoeffizient.r * scalar;
+    outColor.g += lights[i].intensity.g * diffuseCoeffizient.g * scalar;
+    outColor.b += lights[i].intensity.b * diffuseCoeffizient.b * scalar;
   }
 
-  //Reflexionsvektor
-  let reflectVec = new THREE.Vector3().copy(2*scalar.multiplyScalar(scalar) *;);
+  //Spectrum
+  //Reflektionsvektor
+  let reflectionsVec = new THREE.Vector3().copy(normalVec.multiplyScalar(scalar * 2));
+  reflectionsVec.subVectors(reflectionsVec, lightVec);
+  reflectionsVec.normalize();
+  //Viewvektor
+  let viewVec = new THREE.Vector3();
+  viewVec.subVectors(camPosition, position);
+  viewVec.normalize();
+
+  let spectrumScalar = reflectionsVec.dot(viewVec);
+
+  if (spectrumScalar >= 0) {
+    let spekularenAnteil = Math.pow(spectrumScalar,n);
+
+    outColor.r += lights[i].intensity.r * spectrumCoeffizient.r * spekularenAnteil;
+    outColor.g += lights[i].intensity.g * spectrumCoeffizient.g * spekularenAnteil;
+    outColor.b += lights[i].intensity.b * spectrumCoeffizient.b * spekularenAnteil;
+  }
+  }
+
   return outColor;
 }
