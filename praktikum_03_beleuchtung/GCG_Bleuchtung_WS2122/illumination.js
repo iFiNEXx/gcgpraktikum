288ed5ff2ee5ca0e37f70b157e2d7095066d3369
vaki2418 <emil.kiesche@gmail.com>
2022-01-14 12:46:59 +0100

    kleine Korrekturen

--- praktikum_03_beleuchtung/GCG_Bleuchtung_WS2122/illumination.js
+++ praktikum_03_beleuchtung/GCG_Bleuchtung_WS2122/illumination.js
@@ -14,7 +14,7 @@
 // Autor 5    : Lamsalam, Chaymae
 ////////////////////////////////////////////////////////////////////////////////
 
-
+//globale Variablen:
 
 // das ambiente Licht
 let ambientLight = {intensity: {r: 0.125, g: 0.125, b: 0.125}};
@@ -29,13 +29,13 @@ let n = 10;
 
 // alle übrigen (Punkt)-Lichter der Szene
 let lights = [
-  //position 0
+  
   {position: new THREE.Vector3(-100, 100, 75),
    intensity: {r: 0.875, g: 0.625, b: 0.125}},
   {position: new THREE.Vector3(100, -100, 75),
    intensity: {r: 0.5, g: 0.0, b: 0.9}},
-   {position: new THREE.Vector3(100, 100, 75),
-    intensity: {r: 0.1, g: 0.5, b: 0.1}},
+  {position: new THREE.Vector3(100, 100, 75),
+   intensity: {r: 0.1, g: 0.5, b: 0.1}},
 ];
 
 
@@ -53,13 +53,15 @@ function initScene()
 
 
 ////////////////////////////////////////////////////////////////////////////////
+// Implementierung der Beleuchtungsberechnung mit dem Phong Beleuchtungsmodell
 // phong(position, normal, camPosition)
 // Wird während des Renderings für jeden Vertex einmal aufgerufen.
+// 
 // Parameter: position     Vertexposition (Vector3)
 //            normal       Vertexnormale (Vector3)
 //            camPosition  Kameraposition (Vector3)
 // Rueckgabe: Eine Farbe. D.h. ein Objekt mit den Attributen .r, .g und .b
-//
+
 // Hinweis: Alle Parameter befinden sich im selben Koordinatensystem.
 ////////////////////////////////////////////////////////////////////////////////
 function phong(position, normal, camPosition)
@@ -67,9 +69,6 @@ function phong(position, normal, camPosition)
   // Initialisiere den Rueckgabewert
   let outColor = {r: 0.0, g: 0.0, b: 0.0};
   
-  // TODO: Implementieren Sie die Beleuchtungsberechnung
-  //       mit dem Phong-Beleuchtungsmodell.
-
   //Ambient
   //Berechnung der Formel ia = Ia· ka
   outColor.r += ambientLight.intensity.r * ambientCoeffizient.r;
@@ -80,19 +79,18 @@ function phong(position, normal, camPosition)
   for (let i=0; i<lights.length; i++) {
   let lightVec = new THREE.Vector3();
   lightVec.subVectors(lights[i].position, position);
-  // normailisieren
+  // normalisieren
   lightVec.normalize();
   let normalVec = new THREE.Vector3().copy(normal.normalize());
   //Skalarprodukt
   let scalar = normalVec.dot(lightVec);
-
   if (scalar >= 0) {
     outColor.r += lights[i].intensity.r * diffuseCoeffizient.r * scalar;
     outColor.g += lights[i].intensity.g * diffuseCoeffizient.g * scalar;
     outColor.b += lights[i].intensity.b * diffuseCoeffizient.b * scalar;
   }
 
-  //Spectrum
+  //Spektrum
   //Reflektionsvektor
   let reflectionsVec = new THREE.Vector3().copy(normalVec.multiplyScalar(scalar * 2));
   reflectionsVec.subVectors(reflectionsVec, lightVec);
